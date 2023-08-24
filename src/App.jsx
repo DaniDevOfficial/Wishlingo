0f32ff69c678789d1fc970e9892d6c07fde6dd08
David <bischof.david.db@gmail.com>
2023-08-24 21:50:41 +0200

    svg background

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './Styles/App.css'
+function App() {
+  return (
+    <div id="MainBackground">
+wasd  
+      <div className="background-svg1"></div>
+      <div className="background-svg2"></div>
+    </div>
+  );
+}
+
+export default App;
