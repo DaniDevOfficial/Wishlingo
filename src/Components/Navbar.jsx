6107d8f4d5cd78c2b19552a8e8261c659b9219ff
David <bischof.david.db@gmail.com>
2023-08-30 21:20:58 +0200

    Loggin added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebase'; // Adjust the import path according to your project structure
 import '../Styles/Navbar.css';
 
 export function Navbar() {
+    const [user, setUser] = useState(null); // User object or null
+
+    useEffect(() => {
+        // Check if user is logged in
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setUser(user); // Set user object if logged in, or null if not
+        });
+
+        // Cleanup the listener when the component unmounts
+        return () => unsubscribe();
+    }, []);
+
     return (
         <nav className="navbar">
             <div className="left-section">
@@ -11,7 +26,13 @@ export function Navbar() {
             <ul className="nav-links">
                 <li><Link to="/learn">Learn</Link></li>
                 <li><Link to="/contact">Contact</Link></li>
-                <li><Link to="/login">Login</Link></li>
+                <li>
+                    {user ? (
+                        <Link to="/SignIn">Account</Link>
+                    ) : (
+                        <Link to="/SignIn">Login</Link>
+                    )}
+                </li>
             </ul>
         </nav>
     );
--- src/Components/Wrapper.jsx
+++ src/Components/Wrapper.jsx
@@ -7,7 +7,8 @@ import WhatWant from './WhatWant';
 import { Alltopics } from './AllTopics';
 import { SingleTask } from './SingleTask';
 import tasksData from './Tasks/tasks.json';
-
+import { SignIn } from './auth/SignIn';
+import { SignUp } from './auth/SignUp';
 export function Wrapper() {
   const [topics, setTopics] = useState([]);
 
@@ -20,10 +21,13 @@ export function Wrapper() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/SignIn" element={<SignIn />} />
+        <Route path="/SignUp" element={<SignUp />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/learn" element={<WhatKnow />} />
         <Route path="/learn/:lang" element={<WhatWant />} />
         <Route path="/learn/:lang/:learn" element={<Alltopics />} />
+
         
         {topics.map(topic => (
           <Route key={topic} path={`/learn/:lang/:learn/${topic}`} element={<SingleTask />} />
